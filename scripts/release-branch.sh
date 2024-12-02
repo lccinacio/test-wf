a979f662e26f856835089551d6bd17894b758f38
Lucas Inacio <lucas.in@outlook.com>
2024-12-02 14:45:55 -0300

    test

--- scripts/release-branch.sh
+++ scripts/release-branch.sh
@@ -6,7 +6,9 @@ echo "Last tag found: $LAST_TAG"
 VERSION_REGEX="v([0-9]+)\.([0-9]+)\.([0-9]+)"
   if [[ $LAST_TAG =~ $VERSION_REGEX ]]; then
     MAJOR="${BASH_REMATCH[1]}"
+    echo $MAJOR   
     MINOR="${BASH_REMATCH[2]}"
+    echo $MINOR
     PATCH="${BASH_REMATCH[3]}"
   else
     echo "Invalid tag format! Make sure the tag format is 'v<major>.<minor>.<patch>'."
