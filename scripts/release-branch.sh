d255b197ccef52fad928e00013ef61d9b7d95d97
Lucas Inacio <lucas.in@outlook.com>
2024-12-02 14:34:46 -0300

    test

--- scripts/release-branch.sh
+++ scripts/release-branch.sh
@@ -2,6 +2,9 @@ git fetch --tags
 LAST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "Last tag found: $LAST_TAG"
 
-BRANCH_NAME=$(echo $LAST_TAG | sed 's/..$/.x/')
+VERSION=$(echo $LAST_TAG | sed 's/..$/.x/')
+BRANCH_NAME=$(echo $VERSION | sed 's/..#/v')
 
+
+echo $VERSION
 echo $BRANCH_NAME
