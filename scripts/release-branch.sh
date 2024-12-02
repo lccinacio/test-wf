dcc9edabad68ce766697e799572eecd3fd28565c
Lucas Inacio <lucas.in@outlook.com>
2024-12-02 14:29:30 -0300

    test

--- scripts/release-branch.sh
+++ scripts/release-branch.sh
@@ -2,5 +2,5 @@ git fetch --tags
 LAST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "Last tag found: $LAST_TAG"
 
-VERSION=$(^v(.*)\.0$)
-echo $VERSION
+VERSION=$(echo $APP_VERSION | ^v(.*)\.0$)
+echo $APP_VERSION
